8330db1036552ae215ea80d3c6adf1d9ac9c382a
SameerJadav <sameerjadav010@gmail.com>
2023-04-27 12:14:59 +0530

    Transform objects

--- main.js
+++ main.js
@@ -4,11 +4,49 @@ import * as t from 'three';
 // Scene
 const scene = new t.Scene();
 
-// Red cube
-const geometry = new t.BoxGeometry(1, 1, 1);
-const material = new t.MeshBasicMaterial({ color: 0xff0000 });
-const mesh = new t.Mesh(geometry, material);
-scene.add(mesh);
+/**
+ * Axes helper
+ *
+ * const axesHelper = new t.AxesHelper();
+ * scene.add(axesHelper);
+ */
+
+/**
+ * Position and scale are Vector3
+ * Rotation is Euler
+ * We can use set method on both
+ *.set(x, y, z)
+ *
+ * mesh.position.set(0.7, -0.6, 1);
+ * mesh.scale.set(0.5, 0.25, 1.5);
+ * mesh.rotation.set(1, 0.5, 3.14156);
+ */
+
+// Group
+const group = new t.Group();
+group.position.z = 0.5;
+group.rotation.y = 0.5;
+scene.add(group);
+
+const cube1 = new t.Mesh(
+  new t.BoxGeometry(1, 1, 1),
+  new t.MeshBasicMaterial({ color: 0xff0000 })
+);
+group.add(cube1);
+
+const cube2 = new t.Mesh(
+  new t.BoxGeometry(1, 1, 1),
+  new t.MeshBasicMaterial({ color: 0x00ff00 })
+);
+cube2.position.x = -1;
+group.add(cube2);
+
+const cube3 = new t.Mesh(
+  new t.BoxGeometry(1, 1, 1),
+  new t.MeshBasicMaterial({ color: 0x0000ff })
+);
+cube3.position.x = 1;
+group.add(cube3);
 
 // Sizes
 const sizes = {
