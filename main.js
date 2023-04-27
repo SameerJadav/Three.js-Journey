57bab47f40dbe4476c95d158086ad532afef22ce
SameerJadav <sameerjadav010@gmail.com>
2023-04-27 09:40:32 +0530

    Basic scene

--- main.js
+++ main.js
@@ -1,4 +1,30 @@
 import './style.css';
 import * as t from 'three';
 
-console.log(t);
+// Scene
+const scene = new t.Scene();
+
+// Red cube
+const geometry = new t.BoxGeometry(1, 1, 1);
+const material = new t.MeshBasicMaterial({ color: 0xff0000 });
+const mesh = new t.Mesh(geometry, material);
+scene.add(mesh);
+
+// Sizes
+const sizes = {
+  width: 800,
+  height: 600,
+};
+
+// Camera
+const camera = new t.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 3;
+scene.add(camera);
+
+// Renderer
+const canvas = document.querySelector('.webgl');
+const renderer = new t.WebGLRenderer({
+  canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.render(scene, camera);
